856ebb3ba536e3d6fd94498397e7ebf6b2de2bbf
Georg236 <George.zhu@towerswatson.com>
2016-05-21 11:02:57 +1000

    add delegrates and disposer to the class

---
+++ ETrading.Framework/Commands/DelegateCommand.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ETrading.Framework.Commands
+{
+    public interface ICommandCanRaiseEvent : ICommand
+    {
+        /// <summary>
+        /// Raises the CanExecuteChanged event
+        /// </summary>
+        void RaiseCanExecuteChanged();
+    }
+
+    public interface IDelegateCommand : ICommandCanRaiseEvent
+    {
+    }
+
+    public class DelegateCommand : DelegateCommand<object>
+    {
+        /// <summary>
+        /// Create Command with Action and default CanExecute
+        /// </summary>
+        /// <param name="execute"></param>
+        public DelegateCommand(Action execute) : base(execute) { }
+        public DelegateCommand(Action<object> execute) : base(execute) { }
+
+        /// <summary>
+        /// Create Command with Action and CanExecute delegates
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        public DelegateCommand(Action execute, Func<bool> canExecute) : base(notused => execute(), notused => canExecute()) { }
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute) : base(execute, canExecute) { }
+
+        /// <summary>
+        /// Register properties that when changed will raise CanExecuteChanged for this command
+        /// </summary>
+        /// <typeparam name="TNotifier"></typeparam>
+        /// <param name="notifier">Class that implements INotifyPropertyChanged</param>
+        /// <param name="properties">List of property name expressions that when changed will raise CanExecuteChanged</param>
+        /// <returns></returns>
+        public new DelegateCommand RegisterRaiseCanExecuteNotifiers<TNotifier>(TNotifier notifier, params Expression<Func<TNotifier, object>>[] properties) where TNotifier : class, INotifyPropertyChanged
+        {
+            base.RegisterRaiseCanExecuteNotifiers(notifier, properties);
+            return this;
+        }
+    }
+
+    internal class DelegateCommandConfig
+    {
+        static DelegateCommandConfig()
+        {
+            IsForceSafe = ConfigManager.Instance.Get("ForceCommandSafety", true);
+        }
+
+        public static bool IsForceSafe { get; private set; }
+    }
+    public class DelegateCommand<T> : IDelegateCommand
+    {
+        #region Private Fields
+        private readonly Predicate<T> _canExecute;
+        private readonly Action<T> _execute;
+        private WeakEvent<EventHandler> _canExecuteChangedEvent = new WeakEvent<EventHandler>(e => e.h);
+        private readonly Dictionary<WeakReference<INotifyPropertyChanged>, List<string>> _notifyingPropertyList =
+            new Dictionary<WeakReference<INotifyPropertyChanged>, List<string>>();
+
+        #endregion
+
+        /// <summary>
+        /// CanExecuteChanged Event from ICommand, registrations are made weak
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add { _canExecuteChangedEvent += value; }
+            remove { _canExecuteChangedEvent -= value; }
+        }
+
+        #region Constructors
+        protected DelegateCommand(Action execute) : this(notUsed => execute()) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="execute"></param>
+        public DelegateCommand(Action<T> execute)
+            : this(execute, o => true)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        public DelegateCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            if (canExecute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        #endregion
+
+        /// <summary>
+        /// Register properties that when changed will raise CanExecuteChanged for this command
+        /// </summary>
+        /// <typeparam name="TNotifier"></typeparam>
+        /// <param name="notifier">Class that implements INotifyPropertyChanged</param>
+        /// <param name="properties">List of property name expressions that when changed will raise CanExecuteChanged</param>
+        /// <returns></returns>
+        public DelegateCommand<T> RegisterRaiseCanExecuteNotifiers<TNotifier>(TNotifier notifier, params Expression<Func<TNotifier, object>>[] properties) where TNotifier : class, INotifyPropertyChanged
+        {
+            if (notifier == null)
+            {
+                throw new ArgumentNullException("notifier");
+            }
+
+            var propertyList = GetNotifierProperties(notifier);
+
+            if (properties != null && properties.Length > 0)
+            {
+                propertyList.AddRange(properties.Select(e => e.GetPropertyInfo().Name));
+            }
+
+            WeakEvents.Register<INotifyPropertyChanged, PropertyChangedEventHandler>(
+                                notifier, HandleNotifyingObject, (o, e) => o.PropertyChanged += e, (o, e) => o.PropertyChanged -= e);
+
+            return this;
+        }
+
+        private List<string> GetNotifierProperties(INotifyPropertyChanged notifier, bool addIfNull = true)
+        {
+            var key = _notifyingPropertyList.Select(k => k.Key).FirstOrDefault(i => i.Target != null && i.Target.Equals(notifier));
+            if (key == null)
+            {
+                if (addIfNull)
+                {
+                    key = new WeakReference<INotifyPropertyChanged>(notifier);
+                    _notifyingPropertyList[key] = new List<string>();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            return _notifyingPropertyList[key];
+        }
+
+        private void HandleNotifyingObject(object sender, PropertyChangedEventArgs e)
+        {
+            var notifier = sender as INotifyPropertyChanged;
+            if (notifier == null)
+            {
+                return;
+            }
+
+            var propertyList = GetNotifierProperties(notifier, false);
+            if (propertyList == null)
+            {
+                return;
+            }
+
+            if (string.IsNullOrEmpty(e.PropertyName) || propertyList.Count == 0 || propertyList.Any(s => s == e.PropertyName))
+            {
+                var dispatcher = Application.Current != null ? Application.Current.Dispatcher : null;
+                if (dispatcher != null)
+                {
+                    dispatcher.BeginInvoke(RaiseCanExecuteChanged);
+                }
+                else
+                {
+                    RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+
+        [DebuggerStepThrough]
+        public bool CanExecute(T parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        /// <summary>
+        /// Execute the registered Action
+        /// </summary>
+        /// <param name="parameter"></param>
+        [DebuggerStepThrough]
+        public virtual void Execute(T parameter)
+        {
+            _execute(parameter);
+        }
+
+        /// <summary>
+        /// Raises the CanExecuteChanged event
+        /// </summary>
+        [DebuggerStepThrough]
+        public virtual void RaiseCanExecuteChanged()
+        {
+            _canExecuteChangedEvent.Raise(this, EventArgs.Empty);
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            try
+            {
+                return CanExecute((T)parameter);
+            }
+            catch (InvalidCastException)
+            {
+                var methodCallExp = _canExecute.Method;
+                if (DelegateCommandConfig.IsForceSafe)
+                {
+                    return false;
+                }
+                throw;
+            }
+            catch (Exception e)
+            {
+                var methodCallExp = _canExecute.Method;
+                if (DelegateCommandConfig.IsForceSafe)
+                {
+                    //message box service is not available here as MVVM project refers this Project and we want to avoid catastrophic application crash on Command exceptions.
+                    MessageBox.Show("Unknown Error: Refer to the Logs for more details", Strings.ApplicationName, MessageBoxButton.OK, MessageBoxImage.Error);
+                    return false;
+                }
+                throw;
+            }
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            try
+            {
+                Execute((T)parameter);
+            }
+            catch (InvalidCastException)
+            {
+                var methodCallExp = _canExecute.Method;
+                if (!DelegateCommandConfig.IsForceSafe)
+                {
+                    throw;
+                }
+            }
+            catch (Exception e)
+            {
+                var methodCallExp = _execute.Method;
+                if (!DelegateCommandConfig.IsForceSafe)
+                {
+                    throw;
+                }
+                //message box service is not available here as MVVM project refers this Project and we want to avoid catastrophic application crash on Command exceptions.
+                MessageBox.Show("Unknown Error: Refer to the Logs for more details", Strings.ApplicationName, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+
+}
+
+
---
+++ ETrading.Framework/Events/EventBehavior.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Threading;
+using ETrading.Framework.Threading;
+
+namespace ETrading.Framework.Events
+{
+    public class EventBehavior:CommandBehaviorBase<Control>
+    {
+        private EventCommand _bindingInfo;
+        private Delegate _method;
+        private EventInfo _eventInfo;
+        private WeakReference _notifyProperty;
+        private PropertyChangedEventHandler _notifyPropertyHandler;
+
+
+        public EventBehavior(Control control)
+            : base(control)
+        {
+        }
+
+        public void Bind(EventCommand bindingInfo, object dataContext)
+        {
+            _bindingInfo = bindingInfo;
+            ValidateBindingInfo(_bindingInfo);
+            HookEvent();
+            SetCommand(dataContext);
+        }
+
+        public void UnBind()
+        {
+            SetNotifyProperty(null);
+            Command = null;
+            _method = null;
+
+            if (_eventInfo != null)
+            {
+                _eventInfo.RemoveEventHandler(TargetObject, GetEventMethod(_eventInfo));
+                _eventInfo = null;
+            }
+
+        }
+
+        // ReSharper disable UnusedParameter.Local
+        private void ValidateBindingInfo(EventCommand bindingInfo)
+        // ReSharper restore UnusedParameter.Local
+        {
+            if (bindingInfo == null)
+            {
+                throw new ArgumentException("bindingInfo");
+            }
+
+            if (BindingOperations.GetBinding(bindingInfo, EventCommand.CommandProperty) == null)
+            {
+                if (string.IsNullOrEmpty(bindingInfo.CommandName))
+                {
+                    throw new ArgumentException("bindingInfo.CommandName");
+                }
+                if (string.IsNullOrEmpty(bindingInfo.EventName))
+                {
+                    throw new ArgumentException("bindingInfo.EventName");
+                }
+            }
+        }
+
+        private void SetCommand(object dataContext)
+        {
+            if (dataContext == null) return;
+
+
+            Command = _bindingInfo.Command;
+
+            if (Command == null)
+            {
+                var exp = BindingOperations.GetBindingExpressionBase(_bindingInfo, EventCommand.CommandProperty);
+                if (exp != null)
+                {
+                    exp.UpdateTarget();
+                    Command = _bindingInfo.Command;
+                    if (Command == null)
+                    {
+                        //TODO: this is a bit hacky but gets past a binding resolution issue that seems to be related to timing.
+                        //TODO: first found in SelectInputManagerView where binding is null on first execution.  Needs further investigation
+                        _bindingInfo.Dispatcher.BeginInvoke(DispatcherPriority.Background, () =>
+                        {
+                            exp.UpdateTarget();
+                            Command = _bindingInfo.Command;
+                        });
+                    }
+                    return;
+                }
+
+                var propertyInfo = dataContext.GetType().GetProperty(_bindingInfo.CommandName);
+                if (propertyInfo != null)
+                {
+                    Command = propertyInfo.GetValue(dataContext, null) as ICommand;
+                }
+                else
+                {
+                    TrySetCommandToDelegate(dataContext);
+                }
+            }
+        }
+
+        private void TrySetCommandToDelegate(object dataContext)
+        {
+            var method = dataContext.GetType().GetMethod(_bindingInfo.CommandName);
+            if (method != null)
+            {
+
+                var p = method.GetParameters();
+                if (p.Length == 0)
+                {
+                    var can = dataContext.GetType().GetMethod("Can" + _bindingInfo.CommandName);
+                    bool cmdSet = false;
+                    if (can != null)
+                    {
+                        if (can.ReturnType == typeof(bool) && can.GetParameters().Length == 0)
+                        {
+                            cmdSet = true;
+                            Command = new DelegateCommand(o => method.Invoke(dataContext, null),
+                                                          o => (bool)can.Invoke(dataContext, null));
+
+                            SetNotifyProperty(dataContext as INotifyPropertyChanged);
+                        }
+
+                    }
+                    if (!cmdSet)
+                    {
+                        Command = new DelegateCommand(() => method.Invoke(dataContext, null));
+                    }
+
+                }
+                else if (p.Length == 1)
+                {
+                    var can = dataContext.GetType().GetMethod("Can" + _bindingInfo.CommandName);
+                    bool cmdSet = false;
+                    if (can != null)
+                    {
+                        if (can.ReturnType == typeof(bool))
+                        {
+                            cmdSet = true;
+                            Command = new DelegateCommand(o => method.Invoke(dataContext, new[] { _bindingInfo.CommandParameter }),
+                                                          o => (bool)can.Invoke(dataContext, new[] { _bindingInfo.CommandParameter }));
+
+                            SetNotifyProperty(dataContext as INotifyPropertyChanged);
+                        }
+                    }
+                    if (!cmdSet)
+                    {
+                        Command = new DelegateCommand(o =>
+                            method.Invoke(dataContext, new[] { _bindingInfo.CommandParameter }), o => true);
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid Parameters for delegate specified in commandName");
+                }
+            }
+            //KS: Commenting out for temp.
+            //else
+            //{
+            //    throw new ArgumentException("commandName");
+            //}
+        }
+
+        private void HookEvent()
+        {
+            _eventInfo = TargetObject.GetType().GetEvent(_bindingInfo.EventName, BindingFlags.Public | BindingFlags.Instance);
+            if (_eventInfo == null)
+            {
+                throw new ArgumentException("eventName");
+            }
+
+            _eventInfo.RemoveEventHandler(TargetObject, GetEventMethod(_eventInfo));
+            _eventInfo.AddEventHandler(TargetObject, GetEventMethod(_eventInfo));
+        }
+
+        private Delegate GetEventMethod(EventInfo eventInfo)
+        {
+            if (eventInfo == null)
+            {
+                throw new ArgumentNullException("eventInfo");
+            }
+
+            if (eventInfo.EventHandlerType == null)
+            {
+                throw new ArgumentException("EventHandlerType is null");
+            }
+
+            return _method ?? (_method = Delegate.CreateDelegate(
+                eventInfo.EventHandlerType, this,
+                GetType().GetMethod("OnEventRaised", BindingFlags.NonPublic | BindingFlags.Instance)));
+        }
+
+        protected void OnEventRaised(object sender, EventArgs e)
+        {
+
+            if (_bindingInfo.CommandParameter == null)
+            {
+                //TODO - This is a workaround to update the binding commandparameter. We were having problems where the commandparameter was null
+                //on a command fired from selecting an item in a combobox in a datagird (the command was due to 2 way binding not working)
+                //Need to figure out why the binding parameter is null when an binding exists and a parameter should exist. 
+                var binding = BindingOperations.GetBindingExpressionBase(_bindingInfo, EventCommand.CommandParameterProperty);
+                if (binding == null)
+                {
+                    CommandParameter = _bindingInfo.CommandParameter ?? e;
+                }
+                else
+                {
+                    if (_bindingInfo.CommandParameter == null)
+                        binding.UpdateTarget();
+                    CommandParameter = _bindingInfo.CommandParameter;
+                }
+            }
+            else
+            {
+                var bindExpression = BindingOperations.GetBindingExpressionBase(_bindingInfo, EventCommand.CommandParameterProperty);
+                if (bindExpression != null)
+                {
+                    bindExpression.UpdateTarget();
+                    CommandParameter = _bindingInfo.CommandParameter;
+                }
+                else
+                {
+                    CommandParameter = _bindingInfo.CommandParameter;
+                }
+            }
+
+            ExecuteCommand();
+        }
+
+        private void SetNotifyProperty(INotifyPropertyChanged notifyProperty)
+        {
+            INotifyPropertyChanged oldValue = _notifyProperty != null ? _notifyProperty.Target as INotifyPropertyChanged : null;
+
+            if (oldValue != null && _notifyPropertyHandler != null)
+            {
+                oldValue.PropertyChanged -= _notifyPropertyHandler;
+                _notifyProperty = null;
+                _notifyPropertyHandler = null;
+            }
+
+            if (notifyProperty != null)
+            {
+                _notifyProperty = new WeakReference(notifyProperty);
+                _notifyPropertyHandler = WeakEvents.Register<INotifyPropertyChanged, PropertyChangedEventHandler>(
+                                notifyProperty, HandlePropertyChanged, (o, e) => o.PropertyChanged += e, (o, e) => o.PropertyChanged -= e).Handler;
+            }
+        }
+
+        private void HandlePropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (Command != null && Command is IDelegateCommand)
+            {
+                ((IDelegateCommand)Command).RaiseCanExecuteChanged();
+            }
+        }
+    }
+}
+
